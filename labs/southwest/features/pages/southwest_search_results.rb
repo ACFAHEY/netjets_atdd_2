890213a4c171a45cf2d81bfe2fe34dd3945c1698
ACFAHEY <cfahey100@gmail.com>
2015-05-12 17:04:55 -0400

    End of Day 2

---
+++ labs/southwest/features/pages/southwest_search_results.rb
@@ -0,0 +1,15 @@
+require 'page-object'
+
+class SouthwestSearchResults
+  include PageObject
+
+text_field(:departure_airport, :id=>'originAirport_displayed')
+  text_field(:arrival_airport, :id=>'destinationAirport_displayed')
+
+
+  def flights
+      #TODO need to get the actual flight numbers
+       [1234]
+  end
+
+end
---
+++ labs/southwest/features/workflows/southwest_interactions.rb
@@ -0,0 +1,42 @@
+module SouthwestInteractions
+  def search_for_flights_between_valid_airports
+    departure = 'CMH'
+    arrival = 'MCO'
+    on (SouthwestHome) do |page|
+      page.departure_airport = departure
+      page.arrival_airport = arrival
+      page.search
+    end
+    [departure, arrival]
+  end
+
+  def search_for_flights_between_invalid_airports
+    departure = 'CMH'
+    arrival = 'CMH'
+    on (SouthwestHome) do |page|
+      page.departure_airport = departure
+      page.arrival_airport = arrival
+    end
+    [departure, arrival]
+  end
+
+  def goto_southwest_app
+    visit SouthwestHome
+  end
+
+  def search_results_arrival_airport
+    on(SouthwestSearchResults).arrival_airport
+  end
+
+  def search_results_departure_airport
+    on(SouthwestSearchResults).departure_airport
+  end
+
+  def search_results
+    #TODO
+    #['hello']
+    on(SouthwestSearchResults).flights
+  end
+end
+
+World(SouthwestInteractions)
